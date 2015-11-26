945e0f841ce20ed33502ee5dc801b4677c12f666
Tod <tod@8cruz.com>
2015-11-26 22:19:02 +0900

    Input.MousePosition

--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -13,5 +13,22 @@ public class Ground : MonoBehaviour {
         float zRotation = transform.localEulerAngles.z;
         zRotation = zRotation - Input.GetAxis("Horizontal");
         transform.localEulerAngles = new Vector3(10, 0, zRotation);
+
+        if(Input.touchCount>0 || Input.GetMouseButton(0))
+        {
+            Debug.Log("mouse down:"+Input.mousePosition);
+            if (Input.mousePosition.x < Screen.width / 2)
+            {//왼쪽을 클릭
+                transform.localEulerAngles = new Vector3(10
+                    , 0
+                    , transform.localEulerAngles.z+1.0f);
+            }
+            else
+            {//오른쪽을 클릭
+                transform.localEulerAngles = new Vector3(10
+                    , 0
+                    , transform.localEulerAngles.z - 1.0f);
+            }
+        }
 	}
 }
