091afc3217f573bfb68096d407427f91c3269098
Tod <tod@8cruz.com>
2016-04-27 10:43:39 +0900

    7-3 OnTriggerEnter

---
+++ Assets/FailZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FailZone : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.name == "Ball")
+        {
+            Application.LoadLevel("Game");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
