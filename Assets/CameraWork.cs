82bdf63b7ede4403194eed0402d73da17147b214
Tod <tod@8cruz.com>
2016-05-04 10:16:42 +0900

    8-2 Tag

--- Assets/CameraWork.cs
+++ Assets/CameraWork.cs
@@ -6,7 +6,11 @@ public class CameraWork : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ball = GameObject.Find("Ball");
-	}
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+
+        Debug.Log(coins[6].name);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
