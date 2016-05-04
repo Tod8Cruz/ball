ddcef1ccce5dc9bf4714c54368a7934476f730cb
Tod <tod@8cruz.com>
2016-05-04 10:54:05 +0900

    8-3 for문

--- Assets/CameraWork.cs
+++ Assets/CameraWork.cs
@@ -6,9 +6,16 @@ public class CameraWork : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ball = GameObject.Find("Ball");
-        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
 
-        Debug.Log(coins[6].name);
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+        
+
+        for(int i = 0;i < coins.Length; i++)
+        {
+            Debug.Log(coins[i].name);
+        }
+
+
         
     }
 	
