bac3a140c8965c157dce00f227905d15600ae637
Tod <tod@8cruz.com>
2016-05-09 17:39:07 +0900

    8-4 장애물 한 번에 없애기

---
+++ Assets/RedCoin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedCoin : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Ball")
+        {
+
+            DestroyObstacles();
+            Destroy(gameObject);
+        }
+    }
+
+    void DestroyObstacles()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        for(int i = 0; i < obstacles.Length; i++)
+        {
+            Destroy(obstacles[i]);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
